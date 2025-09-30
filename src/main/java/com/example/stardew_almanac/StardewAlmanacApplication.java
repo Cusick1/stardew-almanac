127dd372a893cd39cc68beb2bf1941cc98eac8e9
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-09-29 23:31:33 -0400

    Initial Commit

---
+++ src/main/java/com/example/stardew_almanac/StardewAlmanacApplication.java
@@ -0,0 +1,13 @@
+package com.example.stardew_almanac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StardewAlmanacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StardewAlmanacApplication.class, args);
+	}
+
+}
