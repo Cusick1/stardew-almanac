882bec17af5789462b1e7f7b93d3aa7c3014cb51
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-10-01 22:40:52 -0400

    small updates

--- src/main/java/com/example/stardew_almanac/villagers/VillagerController.java
+++ src/main/java/com/example/stardew_almanac/villagers/VillagerController.java
@@ -1,16 +1,16 @@
 package com.example.stardew_almanac.villagers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 
 @RestController
 public class VillagerController {
 
   private final VillagerService villagerService;
 
+  @Autowired
   VillagerController(VillagerService villagerService) {
     this.villagerService = villagerService;
   }
