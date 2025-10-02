345b7ba77a64af98824e6cc38ad59e0ce2b6c388
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-10-01 22:37:00 -0400

    updated controller to call service

--- src/main/java/com/example/stardew_almanac/crops/CropController.java
+++ src/main/java/com/example/stardew_almanac/crops/CropController.java
@@ -1,8 +1,9 @@
 package com.example.stardew_almanac.crops;
 
-
-
 import com.example.stardew_almanac.common.Season;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,14 +11,15 @@ import java.util.List;
 @RestController
 public class CropController {
 
-    private final List<Crop> crops = List.of(
-            new Crop("Parsnip", Season.SPRING, 4, 8, CropType.VEGETABLE, 18, 0, 35, List.of("Abigail", "Emily"), List.of( "Penny")),
-            new Crop("Cauliflower", com.example.stardew_almanac.common.Season.SPRING, 12, 50, CropType.VEGETABLE, 75, 0, 175, List.of("Haley", "Leah"),
-                    List.of("Alex")));
+  private final CropService cropService;
 
-    public Crop getCropByName(String name) {
-        for(Crop crop : crops)
-            if (crop.getName().equalsIgnoreCase(name)) return crop;
-        return null;
-    }
+  @Autowired
+  public CropController(CropService cropService) {
+    this.cropService = cropService;
+  }
+
+  @GetMapping("/crop")
+  public Crop getCropByName(@RequestParam String name) {
+    return cropService.getCropsByName(name);
+  }
 }
