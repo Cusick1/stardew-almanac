0a23d215657eb3f30c80b3531949a16510c63ae6
Ryan Cusick <38022300+Cusick1@users.noreply.github.com>
2025-10-30 21:53:07 -0400

    removing some unneeded mocking.

--- src/main/java/com/example/stardew_almanac/fish/FishService.java
+++ src/main/java/com/example/stardew_almanac/fish/FishService.java
@@ -4,47 +4,34 @@ import com.example.stardew_almanac.common.Season;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FishService {
 
-  @Autowired private FishRepository fishRepository;
-  private final List<Fish> fishList;
+    @Autowired
+    private FishRepository fishRepository;
 
-  public FishService() {
-    fishList = new ArrayList<>();
-    fishList.add(
-        new Fish(
-            "Bream",
-            "A fairly common river fish that becomes active at night.",
-            "6 PM - 2 AM",
-            "Any",
-            25,
-            75,
-            List.of(Season.ALL),
-            List.of("Town River"),
-            List.of("Fish Taco", "Sashimi")));
-  }
+    public FishService() {
+    }
 
-  public List<Fish> getFish() {
-    return fishRepository.findAll();
-  }
+    public List<Fish> getFish() {
+        return fishRepository.findAll();
+    }
 
-  public Fish getFishByName(String name) {
-    return fishRepository.findById(name).orElse(null);
-  }
+    public Fish getFishByName(String name) {
+        return fishRepository.findById(name).orElse(null);
+    }
 
-  public List<Fish> getFishBySeason(Season season) {
-    return fishRepository.findBySeason(season.toString());
-  }
+    public List<Fish> getFishBySeason(Season season) {
+        return fishRepository.findBySeason(season.toString());
+    }
 
-  public List<Fish> getFishByLocations(String location) {
-    return fishRepository.findByLocations(location);
-  }
+    public List<Fish> getFishByLocations(String location) {
+        return fishRepository.findByLocations(location);
+    }
 
-  public List<Fish> getFishByLocations(String location, Season season) {
-    return fishRepository.findByLocationAndSeason(location, season);
-  }
+    public List<Fish> getFishByLocations(String location, Season season) {
+        return fishRepository.findByLocationAndSeason(location, season);
+    }
 }
